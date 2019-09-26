ff24c88b12cb328a17c48ab42495c6c4ce1c9b89
Sal's Trash Can <odhamtrashcan5@Sals-Mac-Pro.local>
2019-09-26 00:38:03 -0400

    changes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -12,17 +12,6 @@ export default class Sidebar extends Component {
                             <h1 id="colorlib-logo"><a href="index.html">Sal Suriel</a></h1>
                             <span className="email"><i className="icon-mail"></i> surielsa@gmail.com</span>
                         </div>
-                        <nav id="colorlib-main-menu" role="navigation" className="navbar">
-                            <div id="navbar" className="collapse">
-                                <ul>
-                                    <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
-                                    <li><a href="#about" data-nav-section="about">About</a></li>
-                                    {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
-                                    <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
-                                </ul>
-                            </div>
-                        </nav>
                         <nav id="colorlib-main-menu">
                             <ul>
                                 <li><a href="https://github.com/surielsa"><img className="icon" src="assets/images/github-128.png" width={40} height={40} alt="GitHub Profile" target="_blank" /></a></li>
