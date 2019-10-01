19b257f6c10e83997e83a1a8868564178b1b93cc
Sal's Trash Can <odhamtrashcan5@Sals-Mac-Pro.local>
2019-09-30 22:04:55 -0400

    link updates from GitHub

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,14 +8,14 @@ export default class Project extends Component {
                 <h1>Projects</h1>
                 <p> These are complete projects I am proud to have worked on. I also have more in progress in my <span><a href="https://github.com/surielsa"> </a></span>GitHub Page.</p> 
                 
-                <a href="https://surielsa.github.io/word-guess-game/"><img className="icon" src="assets/images/RuPaulThumbnail.jpeg" width={70} height={70} alt="RuPaul's-Drag-Race-Word-Guess-Game" />
+                <a href="https://surielsa.github.io/Rupauls-Drag-Race-word-game/"><img className="icon" src="assets/images/RuPaulThumbnail.jpeg" width={70} height={70} alt="RuPaul's-Drag-Race-Word-Guess-Game" />
                     <p>RuPaul's Drag Race Word Guess Game</p>
-                    <a href="https://surielsa.github.io/word-guess-game/">
+                    <a href="https://surielsa.github.io/Rupauls-Drag-Race-word-game/">
                     </a>
                 </a>
-                <a href="https://surielsa.github.io/unit-4-game/"><img className="icon" src="assets/images/crystalGuessgamethumbnail.jpg" width={70} height={70} alt="Crystals-Collector-Game" />
+                <a href="https://surielsa.github.io/Sals-Crystal-Game/"><img className="icon" src="assets/images/crystalGuessgamethumbnail.jpg" width={70} height={70} alt="Crystals-Collector-Game" />
                     <p>Crystals Collector Game</p>
-                    <a href="https://surielsa.github.io/unit-4-game/">
+                    <a href="https://surielsa.github.io/Sals-Crystal-Game/">
                     </a>
                 </a>
                 <a href="https://surielsa.github.io/liri-node-app/"><img className="icon" src="assets/images/liri-node-app-thumbnail.png" width={70} height={70} alt="liri-node-app" />
